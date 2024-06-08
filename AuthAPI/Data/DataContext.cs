6d8cc28769156462c32f0ea9337f70ca2bb3a666
Jamoliddin Raxmonberdiyev <j.m.raxmonberdiyev@gmail.com>
2024-06-08 20:37:15 +0500

    add datacontext class - AuthAPI

---
+++ AuthAPI/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using AuthAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthAPI.Data
+{
+    public class DataContext : IdentityDbContext<AppUser>
+    {
+        public DataContext(DbContextOptions opt):base(opt)
+        {
+            
+        }
+    }
+}
--- AuthAPI/Program.cs
+++ AuthAPI/Program.cs
@@ -1,6 +1,27 @@
-var builder = WebApplication.CreateBuilder(args);
+using AuthAPI.Data;
+using AuthAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+var connection = builder.Configuration.GetConnectionString("Auth");
+
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseNpgsql(connection);
+});
+builder.Services.AddIdentityCore<AppUser>(opt =>
+{
+    opt.Password.RequireNonAlphanumeric = false;
+    opt.Password.RequireDigit = false;
+    opt.Password.RequireUppercase = false;
+    opt.Password.RequireLowercase = false;
+})
+    .AddRoles<IdentityRole>()
+    .AddRoleManager<RoleManager<IdentityRole>>()
+    .AddEntityFrameworkStores<DataContext>();
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
