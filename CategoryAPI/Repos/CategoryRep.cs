ea4c6dac8254d18417c0812d56fb21f176df78d1
Jamoliddin Raxmonberdiyev <j.m.raxmonberdiyev@gmail.com>
2024-06-10 14:18:18 +0500

    implement CategoryRep

--- CategoryAPI/Repos/CategoryRep.cs
+++ CategoryAPI/Repos/CategoryRep.cs
@@ -1,6 +1,42 @@
-﻿namespace CategoryAPI.Repos
+﻿using CategoryAPI.Data;
+using CategoryAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CategoryAPI.Repos
 {
-    public class CategoryRep
+    public class CategoryRep : ICategoryRep
     {
+        private readonly CategoryDb _db;
+        public CategoryRep(CategoryDb db)
+        {
+            _db = db;
+        }
+        public async Task Create(Category model)
+        {
+            await _db.Categories.AddAsync(model);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task Delete(Category model)
+        {
+            _db.Categories.Remove(model);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Category>> GetAllCategories()
+        {
+            return await _db.Categories.OrderBy(c => c.Title).ToListAsync();
+        }
+
+        public async Task<Category> GetCategoryById(Guid id)
+        {
+            return await _db.Categories.SingleOrDefaultAsync(x=>x.Id == id);
+        }
+
+        public async Task Update(Category model)
+        {
+            _db.Entry(model).State = EntityState.Modified;
+            await _db.SaveChangesAsync();
+        }
     }
 }
--- CategoryAPI/Repos/ICategoryRep.cs
+++ CategoryAPI/Repos/ICategoryRep.cs
@@ -4,12 +4,11 @@ namespace CategoryAPI.Repos
 {
     public interface ICategoryRep
     {
-        Task<Category> GetAllCategories();
+        Task<IEnumerable<Category>> GetAllCategories();
         Task<Category> GetCategoryById(Guid id);
         Task Create(Category model);
         Task Update(Category model);
         Task Delete(Category model);
-        Task<bool> Save();
 
     }
 }
